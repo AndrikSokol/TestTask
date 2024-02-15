a1a4d64ba06b256b36e71af6fed83e83a5e45022
AndrikSokol <AndrikSokol31@gmail.com>
2024-02-15 03:15:11 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            var orderWithMaxAmount = await (from order in _context.Orders
+                                            where (order.Price * order.Quantity) == _context.Orders.Max(o => o.Price * o.Quantity)
+                                            select order)
+                                    .FirstAsync();
+
+            return orderWithMaxAmount;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            List<Order> orders = await (from order in _context.Orders
+                                        where order.Quantity > 10
+                                        select order)
+                                          .ToListAsync();
+
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var userWithMostOrders = await (from user in _context.Users
+                                            join order in _context.Orders on user.Id equals order.UserId into userOrders
+                                            orderby userOrders.Count() descending
+                                            select user)
+                                     .FirstAsync();
+
+            return userWithMostOrders;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var usersInactive = await (from user in _context.Users
+                                       where (user.Status == Enums.UserStatus.Inactive)
+                                       select user)
+                                     .ToListAsync();
+
+            return usersInactive;
+        }
+    }
+}
